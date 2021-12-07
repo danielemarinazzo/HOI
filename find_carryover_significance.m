c864ac0d3dbd2df467e48661d297ab10cb263709
Daniele Marinazzo <daniele.marinazzo@gmail.com>
2021-12-07 15:13:09 +0100

    Update find_carryover_significance.m
    starting from the highest order, otherwise it did not work as it should

--- find_carryover_significance.m
+++ find_carryover_significance.m
@@ -3,19 +3,19 @@ function Otot=find_carryover_significance(Otot)
 % is significant with respect to the value at the lower order
 
 maxsize=length(Otot);
-for isize=3:maxsize-1
-    lower=isize;
-    nlower_red=size(Otot(lower).index_var_red,1);
-    nlower_syn=size(Otot(lower).index_var_syn,1);
-    nhigher_red=size(Otot(lower+1).index_var_red,1);
-    nhigher_syn=size(Otot(lower+1).index_var_syn,1);
+for isize=maxsize:-1:4
+    higher=isize;
+    nlower_red=size(Otot(higher-1).index_var_red,1);
+    nlower_syn=size(Otot(higher-1).index_var_syn,1);
+    nhigher_red=size(Otot(higher).index_var_red,1);
+    nhigher_syn=size(Otot(higher).index_var_syn,1);
     sig_red=zeros(nhigher_red,1);
     sig_syn=zeros(nhigher_syn,1);
     for ilower=1:nlower_red
         for ihigher=1:nhigher_red
-            if sum(ismember(Otot(lower).index_var_red(ilower,:),Otot(lower+1).index_var_red(ihigher,:)))==lower
-                v1=Otot(lower).bootsigCI_red(ilower,:);
-                v2=Otot(lower+1).bootsigCI_red(ihigher,:);
+            if sum(ismember(Otot(higher-1).index_var_red(ilower,:),Otot(higher).index_var_red(ihigher,:)))==higher-1
+                v1=Otot(higher-1).bootsigCI_red(ilower,:);
+                v2=Otot(higher).bootsigCI_red(ihigher,:);
                 l1=v1(1);l2=v2(1);r1=v1(2);r2=v2(2);
                 r = bsxfun(@min, r1, r2);
                 l = bsxfun(@max, l1, l2);
@@ -23,10 +23,10 @@ for isize=3:maxsize-1
             end
         end
     end
-    Otot(isize+1).inc_sig_red=sig_red;
+    Otot(isize).inc_sig_red=sig_red;
     for ilower=1:nlower_syn
         for ihigher=1:nhigher_syn
-            if sum(ismember(Otot(lower).index_var_syn(ilower,:),Otot(lower+1).index_var_syn(ihigher,:)))==lower
+            if sum(ismember(Otot(higher-1).index_var_syn(ilower,:),Otot(higher).index_var_syn(ihigher,:)))==higher-1
                 l1=v1(1);l2=v2(1);r1=v1(2);r2=v2(2);
                 r = bsxfun(@min, r1, r2);
                 l = bsxfun(@max, l1, l2);
@@ -34,5 +34,5 @@ for isize=3:maxsize-1
             end
         end
     end
-    Otot(isize+1).inc_sig_syn=sig_syn;
+    Otot(isize).inc_sig_syn=sig_syn;
 end
