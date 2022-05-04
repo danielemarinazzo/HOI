587c391d906d402fb82a73235e48d447f4325ee7
Daniele Marinazzo <daniele.marinazzo@gmail.com>
2022-05-04 21:41:32 +0200

    changed internal function name

--- exhaustive_loop_lagged_fdr.m
+++ exhaustive_loop_lagged_fdr.m
@@ -1,7 +1,5 @@
-function Otot=exhaustive_loop_lagged_fdr(ts,modelorder,maxsize,n_best,isig)
-if nargin<5
-    isig=0;
-end
+function [Otot_lagged, O_val_size_tot_lagged] = exhaustive_loop_lagged_fdr(ts,modelorder,maxsize,n_best)
+
 Xfull=copnorm(ts);
 %Xfull=X;
 %% Parameters
@@ -11,8 +9,9 @@ X=Xfull;
 nboot = 1000; % number of bootstrap samples
 chunklength = round(N/5); %can play around with this
 alphaval=.05;
-Otot(nvartot,maxsize) = struct();
-
+Otot_lagged(nvartot,maxsize) = struct('index_var_red', [], 'sorted_red', [], 'index_red', [], 'bootsig_red', [], 'bootsigCI_red', [],...
+    'index_var_syn', [], 'sorted_syn', [], 'index_syn', [], 'bootsig_syn', [], 'bootsigCI_syn', []);
+O_val_size_tot_lagged(nvartot,maxsize) = struct('multiplet_value',[]);
 
 %% this section is for the expansion of redundancy, so maximizing the O
 for itarget=1:nvartot
@@ -21,19 +20,20 @@ for itarget=1:nvartot
     for isize = 2:maxsize
         C = nchoosek(setdiff(1:nvartot,itarget),isize);
         ncomb = size(C,1);
-        Osize = zeros(ncomb,1);
+        O_val_size_lagged = zeros(ncomb,1);
         for icomb = 1:ncomb
-%             if mod(icomb,floor(ncomb/10))==1
-%                 fprintf('target %d, size %d, fraction completed %4.2f.\n', itarget, isize, icomb/ncomb);
-%             end
+            %             if mod(icomb,floor(ncomb/10))==1
+            %                 fprintf('target %d, size %d, fraction completed %4.2f.\n', itarget, isize, icomb/ncomb);
+            %             end
             %disp([itarget isize icomb/ncomb])%Y,X,m,indstart,chunklength,indvar
-            Osize(icomb) = o_information_lagged_boot(t,X,modelorder,1:N,0,C(icomb,:));
+            O_val_size_lagged(icomb) = o_information_lagged_boot(t,X,modelorder,1:N,0,C(icomb,:));
         end
+        O_val_size_tot_lagged(itarget,isize).multiplet_val=O_val_size_lagged; % here we save all the values
         %disp('ranking multiplets and bootstrapping')
-        ind_pos = find(Osize > 0);
-        ind_neg = find(Osize < 0);
-        O_pos = Osize(Osize>0);
-        O_neg = Osize(Osize<0);
+        ind_pos = find(O_val_size_lagged > 0);
+        ind_neg = find(O_val_size_lagged < 0);
+        O_pos = O_val_size_lagged(O_val_size_lagged>0);
+        O_neg = O_val_size_lagged(O_val_size_lagged<0);
         [Osort_pos, ind_pos_sort] = sort(O_pos,'descend');
         [Osort_neg, ind_neg_sort] = sort(O_neg);
         if ~isempty(Osort_pos)
@@ -47,9 +47,9 @@ for itarget=1:nvartot
                 boot_sig(isel) = ~((ci(1)<0) && (ci(2)>0));
             end
             h=fdr_bh(p);
-            Otot(itarget,isize).sorted_red = Osort_pos(1:n_sel);
-            Otot(itarget,isize).index_red = ind_pos(ind_pos_sort(1:n_sel));
-            Otot(itarget,isize).bootsig_red = h.*boot_sig;
+            Otot_lagged(itarget,isize).sorted_red = Osort_pos(1:n_sel);
+            Otot_lagged(itarget,isize).index_red = ind_pos(ind_pos_sort(1:n_sel));
+            Otot_lagged(itarget,isize).bootsig_red = h.*boot_sig;
         end
         if ~isempty(Osort_neg)
             n_sel = min(n_best,length(Osort_neg));
@@ -62,21 +62,46 @@ for itarget=1:nvartot
                 boot_sig(isel) = ~((ci(1)<0) && (ci(2)>0));
             end
             h=fdr_bh(p);
-            Otot(itarget,isize).sorted_syn = Osort_neg(1:n_sel);
-            Otot(itarget,isize).index_syn = ind_neg(ind_neg_sort(1:n_sel));
-            Otot(itarget,isize).bootsig_syn = h.*boot_sig;
+            Otot_lagged(itarget,isize).sorted_syn = Osort_neg(1:n_sel);
+            Otot_lagged(itarget,isize).index_syn = ind_neg(ind_neg_sort(1:n_sel));
+            Otot_lagged(itarget,isize).bootsig_syn = h.*boot_sig;
         end
     end
     %toc
 end
+
 %%
-if isig
-    for itarget=1:nvartot
-        for isize=3:maxsize;if ~isempty(Otot(itarget,isize).bootsig_red); Otot(itarget,isize).sorted_red(Otot(itarget,isize).bootsig_red==0)=[];end;end
-        for isize=3:maxsize;if ~isempty(Otot(itarget,isize).bootsig_red); Otot(itarget,isize).index_red(Otot(itarget,isize).bootsig_red==0)=[];end;end
-        for isize=3:maxsize;if ~isempty(Otot(itarget,isize).bootsig_red); Otot(itarget,isize).bootsig_red(Otot(itarget,isize).bootsig_red==0)=[];end;end
-        for isize=3:maxsize;if ~isempty(Otot(itarget,isize).bootsig_syn); Otot(itarget,isize).sorted_syn(Otot(itarget,isize).bootsig_syn==0)=[];end;end
-        for isize=3:maxsize;if ~isempty(Otot(itarget,isize).bootsig_syn); Otot(itarget,isize).index_syn(Otot(itarget,isize).bootsig_syn==0)=[];end;end
-        for isize=3:maxsize;if ~isempty(Otot(itarget,isize).bootsig_syn); Otot(itarget,isize).bootsig_syn(Otot(itarget,isize).bootsig_syn==0)=[];end;end
+for itarget=1:nvartot
+    for isize=3:maxsize;if ~isempty(Otot_lagged(itarget,isize).bootsig_red); Otot_lagged(itarget,isize).sorted_red(Otot_lagged(itarget,isize).bootsig_red==0)=[];end;end
+    for isize=3:maxsize;if ~isempty(Otot_lagged(itarget,isize).bootsig_red); Otot_lagged(itarget,isize).index_red(Otot_lagged(itarget,isize).bootsig_red==0)=[];end;end
+    for isize=3:maxsize;if ~isempty(Otot_lagged(itarget,isize).bootsig_red); Otot_lagged(itarget,isize).bootsig_red(Otot_lagged(itarget,isize).bootsig_red==0)=[];end;end
+    for isize=3:maxsize;if ~isempty(Otot_lagged(itarget,isize).bootsig_syn); Otot_lagged(itarget,isize).sorted_syn(Otot_lagged(itarget,isize).bootsig_syn==0)=[];end;end
+    for isize=3:maxsize;if ~isempty(Otot_lagged(itarget,isize).bootsig_syn); Otot_lagged(itarget,isize).index_syn(Otot_lagged(itarget,isize).bootsig_syn==0)=[];end;end
+    for isize=3:maxsize;if ~isempty(Otot_lagged(itarget,isize).bootsig_syn); Otot_lagged(itarget,isize).bootsig_syn(Otot_lagged(itarget,isize).bootsig_syn==0)=[];end;end
+end
+
+
+%%
+
+Otot_lagged=find_carryover_significance_lagged(Otot_lagged);
+
+for itarget=1:nvartot
+    for isize = 1:maxsize
+        if ~isempty(Otot_lagged(itarget,isize).inc_sig_red)
+            Otot_lagged(itarget,isize).index_var_red(Otot_lagged(itarget,isize).inc_sig_red==0,:)=[];
+            Otot_lagged(itarget,isize).sorted_red(Otot_lagged(itarget,isize).inc_sig_red==0)=[];
+            Otot_lagged(itarget,isize).index_red(Otot_lagged(itarget,isize).inc_sig_red==0)=[];
+            Otot_lagged(itarget,isize).bootsigCI_red(Otot_lagged(itarget,isize).inc_sig_red==0,:)=[];
+            Otot_lagged(itarget,isize).bootsig_red(Otot_lagged(itarget,isize).inc_sig_red==0)=[];
+            Otot_lagged(itarget,isize).inc_sig_red(Otot_lagged(itarget,isize).inc_sig_red==0)=[];
+        end
+        if ~isempty(Otot_lagged(itarget,isize).inc_sig_syn)
+            Otot_lagged(itarget,isize).index_var_syn(Otot_lagged(itarget,isize).inc_sig_syn==0,:)=[];
+            Otot_lagged(itarget,isize).sorted_syn(Otot_lagged(itarget,isize).inc_sig_syn==0)=[];
+            Otot_lagged(itarget,isize).index_syn(Otot_lagged(itarget,isize).inc_sig_syn==0)=[];
+            Otot_lagged(itarget,isize).bootsigCI_syn(Otot_lagged(itarget,isize).inc_sig_syn==0,:)=[];
+            Otot_lagged(itarget,isize).bootsig_syn(Otot_lagged(itarget,isize).inc_sig_syn==0)=[];
+            Otot_lagged(itarget,isize).inc_sig_syn(Otot_lagged(itarget,isize).inc_sig_syn==0)=[];
+        end
     end
 end
